92bbe7f15dddd39c87c666d15784dc7bb7a98c40
Ankur Jha <ankurjha521@gmail.com>
2026-07-22 22:07:32 +0530

    refactor: simplify partnerId type declaration and import in CreateEngineerRequest DTO

--- src/main/java/com/api/batterymantra/dto/user/CreateEngineerRequest.java
+++ src/main/java/com/api/batterymantra/dto/user/CreateEngineerRequest.java
@@ -1,5 +1,7 @@
 package com.api.batterymantra.dto.user;
 
+import java.util.UUID;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -8,7 +10,7 @@ import lombok.Data;
 public class CreateEngineerRequest {
     @NotBlank
     private String firstName;
-    
+
     @NotBlank
     private String lastName;
 
@@ -20,12 +22,12 @@ public class CreateEngineerRequest {
     private String phoneNumber;
 
     private String alternatePhone;
-    
+
     private String address;
     private String city;
 
     @NotBlank
     private String password;
 
-    private java.util.UUID partnerId;
+    private UUID partnerId;
 }
