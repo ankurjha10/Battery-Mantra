b098fe60556e465dfb61b253be2121fb570577f9
Ankur Jha <ankurjha521@gmail.com>
2026-07-23 12:57:28 +0530

    refactor: simplify OrderStatus enum by removing unused statuses

--- src/main/java/com/api/batterymantra/entity/enums/OrderStatus.java
+++ src/main/java/com/api/batterymantra/entity/enums/OrderStatus.java
@@ -2,12 +2,8 @@ package com.api.batterymantra.entity.enums;
 
 public enum OrderStatus {
     PENDING,
-    CONFIRMED,
     PROCESSING,
     SHIPPED,
-    OUT_FOR_DELIVERY,
     DELIVERED,
-    INSTALLED,
-    CANCELLED,
-    RETURNED
+    CANCELLED
 }
