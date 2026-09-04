33b2ca55a9c71f631a39425e8efbc6d5008f091b
Ankur Jha <ankurjha521@gmail.com>
2026-09-04 20:24:33 +0530

    refactor: make serialNumber field optional in EngineerCompleteJobRequest DTO

--- src/main/java/com/api/batterymantra/dto/engineer/EngineerCompleteJobRequest.java
+++ src/main/java/com/api/batterymantra/dto/engineer/EngineerCompleteJobRequest.java
@@ -9,7 +9,6 @@ public class EngineerCompleteJobRequest {
     @NotBlank(message = "OTP is required")
     private String otp;
 
-    @NotBlank(message = "Serial Number is required")
     private String serialNumber;
 
     private boolean oldBatteryCollected;
